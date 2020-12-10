83b0249c6a0b064e44dfa6fe44107e52084f09a6
Bruno Sajermann <contato@sajermann.com>
2020-12-09 22:28:10 -0300

    Video 3 Complete

---
+++ CursoMVC/CursoMVC/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=localhost;Database=CursoMvc;User Id=sa;pwd=Bruno1234;Integrated Security=False");
+        }
+    }
+}
